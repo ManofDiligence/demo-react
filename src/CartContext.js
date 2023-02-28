17cbffca387a56e33ce10d36dc241413f23fa44e
ManofDiligence <88470511+ManofDiligence@users.noreply.github.com>
2023-03-01 00:52:25 +0800

    react-v1

---
+++ src/CartContext.js
@@ -0,0 +1,5 @@
+import { createContext } from "react";
+// first declare a variable which store the createContext()
+// go to the App() to declare the coverage of useContext() in this app
+// using <CartContext.Provider></CartContext.Provider>
+export const CartContext = createContext();
